46a218e0601e3cbbc3e2475f52ff6cf92ca4c6c0
tbeede <trrbeede@gmail.com>
2019-06-02 18:51:28 -0400

    add folder logic

--- features/pages/settings_page.py
+++ features/pages/settings_page.py
@@ -11,9 +11,9 @@ class SettingsPageElements(object):
     ACCOUNT_BUTTON = '#gb > div.gb_pd.gb_Fd.gb_xd.gb_5b > div.gb_wc.gb_Ka.gb_vc.gb_Dd > div > div.gb_Fa.gb_Qc.gb_bg.gb_f.gb_kf > div > a'
 
     # folder elements
-    ADD_FOLDER = '//*[@id=":7h"]'
+    ADD_FOLDER = '//*[contains(text(), "Create new label")]' # '//*[@id=":7h"]'
     FOLDER_NAME = '//*[@id=":7p.na"]'
-    FOLDER_SUBMIT = 'ok' # 'body > div.Kj-JD > div.Kj-JD-Jl > button.J-at1-auR'
+    FOLDER_SUBMIT = '/html/body/div[27]/div[3]/button[1]'
     FOLDER_SUCCESS = 'body > div:nth-child(18) > div.nH > div > div.nH.w-asV.aiw > div:nth-child(6) > div.no > div > div:nth-child(3) > div > div > div.vh > span > span'
     FOLDER_DELETE = '//*[@id=":89"]/div'
     FOLDER_DELETE_BUTTON = '/html/body/div[27]/div[3]/button[1]'
@@ -36,17 +36,17 @@ class SettingsPage(Browser):
             EC.presence_of_element_located((By.CSS_SELECTOR, SettingsPageElements.SETTINGS_TITLE))
         )
         self.driver.execute_script('window.scrollTo(0, 1080)')
-        create_folder_button = self.driver.find_element_by_xpath(SettingsPageElements.ADD_FOLDER)
+        create_folder_button = self.driver.find_element_by_link_text(SettingsPageElements.ADD_FOLDER)
         create_folder_button.click()
 
         self.driver.find_element_by_xpath(SettingsPageElements.FOLDER_NAME).send_keys(name)
-        self.driver.send_keys(Keys.ENTER)
-        # self.driver.find_element_by_xpath(SettingsPageElements.FOLDER_SUBMIT).click()
+        self.driver.find_element_by_xpath(SettingsPageElements.FOLDER_SUBMIT).click()
 
     def delete_folder(self):
         self.driver.find_element_by_xpath(SettingsPageElements.FOLDER_DELETE).click()
         self.driver.find_element_by_xpath(SettingsPageElements.FOLDER_DELETE_BUTTON).click()
 
     def folder_success(self):
-        self.driver.find_element_by_css_selector(SettingsPageElements.FOLDER_SUCCESS)
+        self.driver.find_element_by_link_text('Test Folder').click()
+        # self.driver.find_element_by_css_selector(SettingsPageElements.FOLDER_SUCCESS)
 
