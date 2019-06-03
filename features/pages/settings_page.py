84f4fb60ee6151499308cbf54303cc340c3d9974
tbeede <trrbeede@gmail.com>
2019-06-02 20:12:13 -0400

    add logic to settings page object

--- features/pages/settings_page.py
+++ features/pages/settings_page.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 
 from features.browser import Browser
 
@@ -11,12 +12,12 @@ class SettingsPageElements(object):
     ACCOUNT_BUTTON = '#gb > div.gb_pd.gb_Fd.gb_xd.gb_5b > div.gb_wc.gb_Ka.gb_vc.gb_Dd > div > div.gb_Fa.gb_Qc.gb_bg.gb_f.gb_kf > div > a'
 
     # folder elements
-    ADD_FOLDER = '//*[contains(text(), "Create new label")]' # '//*[@id=":7h"]'
-    FOLDER_NAME = '//*[@id=":7p.na"]'
-    FOLDER_SUBMIT = '/html/body/div[27]/div[3]/button[1]'
-    FOLDER_SUCCESS = 'body > div:nth-child(18) > div.nH > div > div.nH.w-asV.aiw > div:nth-child(6) > div.no > div > div:nth-child(3) > div > div > div.vh > span > span'
+    ADD_FOLDER = '//*[contains(text(), "Create new label")]'
+    FOLDER_SUBMIT = 'ok'
+    FOLDER_SUCCESS = 'https://mail.google.com/mail/u/0/#label/Test+Folder'
     FOLDER_DELETE = '//*[@id=":89"]/div'
     FOLDER_DELETE_BUTTON = '/html/body/div[27]/div[3]/button[1]'
+    MOVE_TO = '//*[contains(text(), "Move to"'
 
     # settings elements
     SETTINGS_TITLE = '#\:4 > div > div.nH.qZ.G-atb > div.du > h2'
@@ -36,17 +37,20 @@ class SettingsPage(Browser):
             EC.presence_of_element_located((By.CSS_SELECTOR, SettingsPageElements.SETTINGS_TITLE))
         )
         self.driver.execute_script('window.scrollTo(0, 1080)')
-        create_folder_button = self.driver.find_element_by_link_text(SettingsPageElements.ADD_FOLDER)
+        create_folder_button = self.driver.find_element_by_xpath(SettingsPageElements.ADD_FOLDER)
         create_folder_button.click()
 
-        self.driver.find_element_by_xpath(SettingsPageElements.FOLDER_NAME).send_keys(name)
-        self.driver.find_element_by_xpath(SettingsPageElements.FOLDER_SUBMIT).click()
+        actions = ActionChains(self.driver)
+        actions.send_keys(name)
+        # actions.perform()
+
+        self.driver.find_element_by_name(SettingsPageElements.FOLDER_SUBMIT).click()
 
     def delete_folder(self):
         self.driver.find_element_by_xpath(SettingsPageElements.FOLDER_DELETE).click()
         self.driver.find_element_by_xpath(SettingsPageElements.FOLDER_DELETE_BUTTON).click()
 
     def folder_success(self):
-        self.driver.find_element_by_link_text('Test Folder').click()
+        self.driver.get(SettingsPageElements.FOLDER_SUCCESS)
         # self.driver.find_element_by_css_selector(SettingsPageElements.FOLDER_SUCCESS)
 
